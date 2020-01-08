598b3ab78b4fbca07f8be3d1f66b375dc52973d9
Kevin Bedin <kbedin@iri.upc.edu>
2020-01-08 15:40:20 +0100

    crackDetection upgraded

--- workspaceRos/src/phantomx/phantomx_mission_completion/src/crackDetection.py
+++ workspaceRos/src/phantomx/phantomx_mission_completion/src/crackDetection.py
@@ -10,32 +10,36 @@ import matplotlib.pyplot as plt
 def preprocessing(img):
     """
         Input:
-            img: BGRD
+            img: RGBD
         Output:
             new_img: image in GrayLevel
             distance: D channel of the input img
     """
-    bgr=img[:,:,:3]
+    print(img.shape)
+    rgb=img[:,:,:3]
+    bgr = rgb[:, :, ::-1]
     distance=img[:,:-1]
     new_img =  cv.cvtColor(bgr, cv.COLOR_BGR2GRAY)
     return(new_img,distance)
 
-def isCrack(img,dist,thres=0.15):
+def isCrack(img,dist,thresh=0.15,thresh_level=30):
     """
         Input:
             img : GrayLevel
             dist : Matrix of distance of each pixel of img
-            thres : Threshold to detect or not a crack from its histograme larger
+            thresh : Threshold to detect or not a crack from its histograme larger
+            thresh_level : Threshold level for binarization
 
         Output:
             crack_dist : None if no crack detection, distance of the crack (img) if detection
     """
     hist = cv.calcHist([img], [0], None, [256], [0, 256])
     larger_hist=np.std(hist>1)
-    if (larger_hist<thres):
+    if (larger_hist<thresh):
         return None
     else:
-        ret,th = cv.threshold(img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+#        th = cv.threshold(img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+        th = img<thresh_level
         crack_dist=th*dist
         return(crack_dist)
 
@@ -48,15 +52,17 @@ if __name__ == '__main__':
 
     img = cv.imread(PATH+'Capture d’écran de 2020-01-08 10-55-36.png')
     #    img = cv.imread(PATH+'Capture d’écran de 2020-01-08 10-55-48_SANS_FISSURE.png')
+    img = cv.imread(PATH+'image.png')
+
+    img,dist=preprocessing(img)
 
     plt.figure()
-    plt.imshow(img)
-    img,dist=preprocessing(img)
+    plt.imshow(img,cmap="gray")
     crack_dist=isCrack(img,img)
     if crack_dist is not None:
         print("Crack detection")
         plt.figure()
-        plt.imshow(crack_dist)
+        plt.imshow(crack_dist,cmap="gray")
     else:
         print("No crack detection")
     plt.show()
